5884e750e83f0b394b0ac7a502ceed5c977e62a1
elevne <97422844+elevne@users.noreply.github.com>
2022-04-20 16:11:26 +0900

    Add files via upload

---
+++ 추가스터디1주차/5번강의.js
@@ -0,0 +1,20 @@
+let integer = 10; // 10진수
+let hex = 0xa; // 16wlstn
+let binary = 0b1010 // 2진수
+let octal = 0o12; // 8진수
+
+console.log(integer, hex, binary, octal)
+
+let negative = -10;
+let indices = 1.0e1; // 지수
+let double = 10.12 // 소수
+
+console.log(negative, double, indices);
+
+let IsInfinity = 10 / 0;
+let IsNaN = 10 / "칠";
+
+console.log(IsInfinity, IsNaN);
+
+let sum = 0.1+0.2;
+console.log(sum);
