63bb3acd3cf2d024d92b5803949ed76e32cb7eff
elevne <97422844+elevne@users.noreply.github.com>
2022-04-20 16:03:54 +0900

    Add files via upload

---
+++ 추가스터디1주차/4번강의.js
@@ -0,0 +1,25 @@
+// 원시타입
+let number = 1; // 숫자형
+let str = 'abc' // 문자열형
+let bool = false; // 논리형
+let undi = undefined; // undefined
+let nul = null; // null
+let symbol = Symbol(); // symbol
+
+
+console.log(typeof(1));
+console.log(typeof(str));
+console.log(typeof(bool));
+console.log(typeof(undi));
+console.log(typeof(nul));
+console.log(typeof(symbol));
+
+// 참조타입
+
+let array = []; // 배열(object)
+let obj = {}; // 객체(object)
+let func = function(){}; // 함수
+
+console.log(typeof(array));
+console.log(typeof(obj));
+console.log(typeof(func));
