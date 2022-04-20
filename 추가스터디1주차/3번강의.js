f96ad149ec324bfa8384191bfade87b1123883de
elevne <97422844+elevne@users.noreply.github.com>
2022-04-20 15:55:42 +0900

    Add files via upload

---
+++ 추가스터디1주차/3번강의.js
@@ -0,0 +1,26 @@
+num = 3;
+console.log(num);
+
+var num = 3;
+console.log(num);
+
+let str = "a";
+console.log(str)
+
+const constant = 10;
+console.log(constant);
+
+// 변수 이름은 camelcase로 작성
+let userName = "sucoding";
+
+// 변수 이름은 문자, _, $로 시작
+let day12 = "월요일";
+let _day12 = "월요일";
+let $day12 = "월요일";
+
+// 상수나 축약어는 대문자와 스네이크케이스 방식으로 작성\
+const HTML = "Hyper Text Markup Language";
+const MAX_LEVEL = "99";
+
+// 예약어는 사용할 수 없다. var, let, const, typeof 등등
+const var = 10; // 안됨!!
