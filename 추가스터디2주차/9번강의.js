d1bcb8752139fa5429a773d1217c337ecc8dd625
elevne <97422844+elevne@users.noreply.github.com>
2022-04-27 14:58:57 +0900

    Add files via upload

---
+++ 추가스터디2주차/9번강의.js
@@ -0,0 +1,12 @@
+let arr = [];
+console.log(arr);
+
+let arr0 = [10];
+console.log(arr0);
+
+let arr1 = [10, 20];
+console.log(arr1);
+
+let arr2 = [10, 'abc', true, null, undefined, function(){}, {}];
+console.log(arr2);
+console.log(arr2[3])
