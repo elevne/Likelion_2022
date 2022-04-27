35902efdbc3ec84167254edbdc11c03314836bae
elevne <97422844+elevne@users.noreply.github.com>
2022-04-27 15:05:50 +0900

    Add files via upload

---
+++ 추가스터디2주차/10번강의.js
@@ -0,0 +1,11 @@
+let student = [90, 70, 80, 60]; // 배열 : 직관적이지 않다는 단점
+
+let student1 = {
+    koreanScore:90,
+    englishScore:70,
+    mathScore:80,
+    scienceScore:60
+}; // 객체는 key 값이 있어 더 직관적
+
+console.log(student1['koreanScore']);
+console.log(student1.englishScore);
