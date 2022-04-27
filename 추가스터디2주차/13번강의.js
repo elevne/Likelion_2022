5080b45aec2b82374f8af7922c2efdef97b2cc80
elevne <97422844+elevne@users.noreply.github.com>
2022-04-27 15:23:33 +0900

    Add files via upload

---
+++ 추가스터디2주차/13번강의.js
@@ -0,0 +1,15 @@
+function sum(num1, num2){
+    return num1 + num2;
+}
+
+console.log(sum(15, 20));
+
+
+
+const sum_result_1 = sum(10, 20);
+console.log(sum_result_1);
+
+const sum_result_2 = sum(30, 40);
+const sum_result = sum_result_1 + sum_result_2;
+
+console.log(sum_result);
