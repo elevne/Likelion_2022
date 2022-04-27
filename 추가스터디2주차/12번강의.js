e2a1e5a12c845ed87c4576bfd7756036a24b34fa
elevne <97422844+elevne@users.noreply.github.com>
2022-04-27 15:19:18 +0900

    Add files via upload

---
+++ 추가스터디2주차/12번강의.js
@@ -0,0 +1,33 @@
+function printBanana(){
+    console.log('banana');
+}
+
+printBanana();
+
+function printApple(){
+    console.log('apple');
+}
+
+printApple();
+
+
+function printFruit(name){
+    console.log(name);
+}
+printFruit('bananan');
+printFruit('apple');
+
+function printFruits(name, price){
+    console.log(name+'는 '+price+'원 입니다.');
+}
+printFruits('banana', 2000);
+
+function printArr(arr){
+    console.log(arr[0]+'은 '+arr[1];)
+}
+printArr(['banana', 2000]);
+
+function printObj(obj){
+    console.log(obj.name+'은 '+obj.price);
+}
+printObj({name:'apple', price:3000});
