b0470d3b62a6b400f60d0c0a62c753a7c37799f7
elevne <97422844+elevne@users.noreply.github.com>
2022-04-27 15:12:14 +0900

    Add files via upload

---
+++ 추가스터디2주차/11번강의.js
@@ -0,0 +1,14 @@
+const gugudan = function ddd(){
+    console.log('3 * 1 = 3');
+    console.log('3 * 2 = 3');
+    console.log('3 * 3 = 3');
+    console.log('3 * 4 = 3');
+    console.log('3 * 5 = 3');
+    console.log('3 * 6 = 3');
+    console.log('3 * 7 = 3');
+    console.log('3 * 8 = 3');
+    console.log('3 * 9 = 3');
+};
+
+gugudan();
+gugudan();
